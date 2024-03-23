041c155c6bd494094db34749f19f7d474bc23715
Anna Powaska <annapowaska@op.pl>
2024-03-23 06:29:48 +0100

    .

---
+++ src/subpages/shops/shops.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const allShops = document.querySelectorAll('.allShops')
+
+
+function shopsSlide(){
+
+}
+
+shopsSlide()
+
+
+
+function shopsShowHandler(){
+    allLinks = document.querySelectorAll('.allLinks a')
+    for(link of allLinks){
+        link.addE()
+
+    }
+}
+shopsShowHandler()
