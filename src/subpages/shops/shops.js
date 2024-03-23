6db924eb6056f408bff7031b17f08885a172aa57
Anna Powaska <annapowaska@op.pl>
2024-03-23 08:32:07 +0100

    .

--- src/subpages/shops/shops.js
+++ src/subpages/shops/shops.js
@@ -1,21 +1,34 @@
 'use strict';
 
-const allShops = document.querySelectorAll('.allShops')
+function titleClickHandler(event){
+  console.log('Link was clicked!');
+event.preventDefault()
+const clickedEl = this;
+  /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a');
 
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
+  /* add class 'active' to the clicked link */
+clickedEl.classList.add('active')
+  /* remove class 'active' from all articles */
+  const activeLArt = document.querySelectorAll('.allShops .post');
 
-function shopsSlide(){
-
+  for(let activeA of activeLArt){
+    activeA.classList.remove('active');
+  }
+  /* get 'href' attribute from the clicked link */
+const href = clickedEl.getAttribute('href')
+console.log(href)
+  /* find the correct article using the selector (value of 'href' attribute) */
+const match =  document.querySelector(href)
+  /* add class 'active' to the correct article */
+  match.classList.add('active');
 }
 
-shopsSlide()
+const links = document.querySelectorAll('.titles a');
 
-
-
-function shopsShowHandler(){
-    allLinks = document.querySelectorAll('.allLinks a')
-    for(link of allLinks){
-        link.addE()
-
-    }
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
 }
-shopsShowHandler()
