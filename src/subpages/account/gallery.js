be93ac042fe3cf2d9f7c08d138c17744f5781568
Anna Powaska <annapowaska@op.pl>
2024-04-10 11:16:46 +0200

    API connected for orders

--- src/subpages/account/gallery.js
+++ src/subpages/account/gallery.js
@@ -5,3 +5,25 @@ function cb(x){
 function rest(){
     $('.element').css({'background':'blue'});
 }
+
+
+
+const products = [
+    {
+      id: 1, 
+      name: 'Pencil', 
+      attributes: {
+        material: 'wood/graphite',
+      }, 
+    },
+    {
+      id: 2, 
+      name: 'Pen', 
+      attributes: {
+        material: 'metal/plastic',
+      },
+    },
+  ];
+
+  const ordersAPI = JSON.stringify(products)
+  console.log(ordersAPI)
