7cb3c143c0dab6c07bea85748f3a86329ca5bbc1
Anna Powaska <annapowaska@op.pl>
2024-04-13 07:47:26 +0200

    Local storage/but not working yet

--- src/subpages/lifestyle/lifestyle.js
+++ src/subpages/lifestyle/lifestyle.js
@@ -18,3 +18,24 @@ document.getElementById("btn-post").addEventListener("click", function (event) {
     // Clear the textarea after posting the comment
     document.getElementById("new-comment").value = "";
 });
+function showInput() {
+    document.getElementById('comment-container').innerHTML =
+      document.getElementById("new-comment").value;
+  }
+const localStorageKey = "myData";
+document.getElementById("new-comment").value = getDataFromLocalStorage();
+document.getElementById('comment-container').innerHTML = getDataFromLocalStorage();
+
+function showInput() {
+  var user_input = document.getElementById("new-comment").value;
+  setDataToLocalStorage(user_input);
+  document.getElementById('comment-container').innerHTML = user_input;
+}
+
+function setDataToLocalStorage(newData){
+   localStorage.setItem(localStorageKey, newData);
+}
+
+function getDataFromLocalStorage(){
+  return localStorage.getItem(localStorageKey) || "";
+}
