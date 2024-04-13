efe3c4bd7cfcfd553a70cfec64005822e2374037
Anna Powaska <annapowaska@op.pl>
2024-04-13 08:47:21 +0200

    Cleanup

--- src/subpages/lifestyle/lifestyle.js
+++ src/subpages/lifestyle/lifestyle.js
@@ -1,21 +1,12 @@
-// Step 1: Add an event listener to the "Post" button
+
 document.getElementById("btn-post").addEventListener("click", function (event) {
     event.preventDefault()
-    // This line sets up an event listener for the click event on the "Post" button.
-    // When the button is clicked, the function provided as the second argument will run.
-
-    // Step 2: Get the comment from the textarea
-    const newComment = document.getElementById("new-comment").value;
-
-    // Step 3: Create a new paragraph element for the comment
+   
+    const newComment = document.getElementById("new-comment").value
     const commentContainer = document.getElementById("comment-container");
     const commentElement = document.createElement("p");
-    commentElement.innerHTML = '<p class="newComment">Anonim: </p>' + newComment;
-
-    // Step 4: Append the comment to the comment container
+    commentElement.innerHTML = '<p class="newComment"><i class="fa-solid fa-user"></i> Anonim: </p>' + newComment;
     commentContainer.appendChild(commentElement);
-
-    // Clear the textarea after posting the comment
     document.getElementById("new-comment").value = "";
 });
 function showInput() {
