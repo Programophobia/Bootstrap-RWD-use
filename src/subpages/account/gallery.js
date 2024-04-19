232d912bc83238e212937840f7f56bce50e14d06
Anna Powaska <annapowaska@op.pl>
2024-04-19 08:09:58 +0200

    Tag

--- src/subpages/account/gallery.js
+++ src/subpages/account/gallery.js
@@ -27,3 +27,18 @@ const products = [
 
   const ordersAPI = JSON.stringify(products)
   console.log(ordersAPI)
+
+  function printHeart(event){
+event.preventDefault();
+const thisPic = this;
+thisPic.classList.toggle('a');
+const here = thisPic.querySelector('.heart')
+here.innerHTML = '<div id="k"><i class="fa-solid fa-heart"></i></div>'
+const hey = document.getElementById("k");
+hey.classList.remove('j')
+  }
+
+  const pics = document.querySelectorAll('.element')
+  for(let pi of pics){
+    pi.addEventListener('click', printHeart)
+  }
