8019dfbae502af244ea003323db147ed30e32d2a
Anna Powaska <annapowaska@op.pl>
2024-05-02 12:55:33 +0200

    Date

--- src/subpages/belts/products.js
+++ src/subpages/belts/products.js
@@ -20,8 +20,8 @@ const sentOrderButton = document.querySelector('.API-getaway');
 sentOrderButton.addEventListener('click', sendOrder)
 
 //flatpick
-//const element = document.querySelector('.delivery-date')
-//flatpickr(element, {});
+const element = document.querySelector('.delivery-date')
+flatpickr(element, {});
 
 function sendOrder() {
   const thisCart = this;
